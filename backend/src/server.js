0f542943d2386894cf11cd4804ca02386d1a7960
student Preetivaza <preetivaza@gmail.com>
2025-12-29 20:53:45 +0530

    final

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,7 +22,7 @@ const app = express();
 connectDB();
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000', 'http://localhost:3001'],
   credentials: true
 }));
 
